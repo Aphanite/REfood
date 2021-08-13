ece43073d5f3bc38b0d1c2640faaec38285dc00b
Ena Rajovic <ena.rajovic@online.de>
2021-08-13 15:56:59 +0200

    adding Schedule model + fixing associations syntax

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many: ordered_items
-  has_many: products through: ordered_items
+  has_many :ordered_items
+  has_many :products, through: :ordered_items
 end
--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,5 +1,7 @@
 class Supermarket < ApplicationRecord
-  has_many: products
-  has_many: supermarket_reviews
-  has_many: ordered_items through: products
+  has_one :schedule
+  has_many  :products
+  has_many :supermarket_reviews
+  has_many :ordered_items, through: :products
+
 end
