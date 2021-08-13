bb6ae168c8fd4adddfe710d4be2e8e3768d63d3e
Ena Rajovic <ena.rajovic@online.de>
2021-08-13 15:49:23 +0200

    adding associations to models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many: ordered_items
+  has_many: products through: ordered_items
 end
--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,2 +1,5 @@
 class Supermarket < ApplicationRecord
+  has_many: products
+  has_many: supermarket_reviews
+  has_many: ordered_items through: products
 end
