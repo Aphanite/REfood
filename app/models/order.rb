bd644bca75b3e7b80935990f10c994027542206c
Ena Rajovic <ena.rajovic@online.de>
2021-08-13 17:26:29 +0200

    making status enum for Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :ordered_items
   has_many :products, through: :ordered_items
+
+  enum status: { pending: 0, confirmed: 1, declined: 2 }
 end
--- db/migrate/20210813131321_create_orders.rb
+++ db/migrate/20210813131321_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.string :status, default: "pending"
+      t.integer :status, default: 0
       t.integer :amount
       t.date :date
       t.references :user, null: false, foreign_key: true
