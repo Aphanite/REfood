1e7dd1ab0929db12e371ae0ef479839e8a19d337
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-08-14 16:46:17 +0200

    creating seed for Supermarket

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # cleaning the database for Users, Supermarkets, Products
 puts "cleaning the database"
 # Product.destroy_all
-# Supermarket.destroy_all
+Supermarket.destroy_all
 User.destroy_all
 
 # creating User instances
@@ -12,4 +12,21 @@ puts "Finished creating #{User.count} new Users"
 puts ""
 
 # creating Supermarkets instances
+Supermarket.create!(name: "Rewe", address: "Friedrichstraße 100, Berlin")
+Supermarket.create!(name: "Rewe", address: "Tempelhofer Weg 11, Berlin")
+Supermarket.create!(name: "Rewe", address: "Friedrichstraße 60, Berlin")
+Supermarket.create!(name: "Alnatura", address: "Schönhauser Allee 108, Berlin")
+Supermarket.create!(name: "Alnatura", address: "Friedrichstraße 191, Berlin")
+Supermarket.create!(name: "Alnatura", address: "Giesebrechtstraße 4, Berlin")
+Supermarket.create!(name: "Aldi Süd", address: "Badstraße 4, Berlin")
+Supermarket.create!(name: "Aldi Süd", address: "Müllerstraße 128, Berlin")
+Supermarket.create!(name: "Aldi Süd", address: "Schwedter Str. 83, Berlin")
+Supermarket.create!(name: "Lidl", address: "Leipziger Str. 42, Berlin")
+Supermarket.create!(name: "Lidl", address: "Knesebeckstraße 47/48, Berlin")
+Supermarket.create!(name: "Lidl", address: "Heinrich-Heine-Straße 30, Berlin")
+
+puts "Finished creating #{Supermarket.count} new Supermarkets"
+puts ""
+
+# creating Products instances
 
