2e1ec1c5252fc77a9cbdb9dd2193f9766819e2ff
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-08-14 15:57:06 +0200

    initializing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# cleaning the database for Users, Supermarkets, Products
+puts "cleaning the database"
+Product.destroy_all
+Supermarket.destroy_all
+User.destroy_all
+
+# creating User instances
+
+
