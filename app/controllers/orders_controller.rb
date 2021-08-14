d11ee3ad90f4add6934337b53d7ac9d3a4e0b848
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-08-14 11:52:08 +0200

    adding actions for controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+  end
+
+  def order_params
+    params.require(:order).permit(:status, :amount, :date)
+  end
 end
