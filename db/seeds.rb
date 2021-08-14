dad4824c635abad6dc04c2d6aaffcc7abfeb9c07
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-08-14 16:18:20 +0200

    creating Users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 # cleaning the database for Users, Supermarkets, Products
 puts "cleaning the database"
-Product.destroy_all
-Supermarket.destroy_all
+# Product.destroy_all
+# Supermarket.destroy_all
 User.destroy_all
 
 # creating User instances
+30.times do 
+    User.create!(name: Faker::Name.unique.name, email: Faker::Internet.email(domain: "example.com"), password: Faker::Internet.password(min_length: 6, max_length: 8), points: rand(1..100))
+end
+puts "Finished creating #{User.count} new Users"
+puts ""
 
+# creating Supermarkets instances
 
