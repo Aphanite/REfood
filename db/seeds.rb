55aa1fed74dd544d274fb6febf8edea525a26058
Zuzana Soltisova <zuzanasoltisova1@gmail.com>
2021-08-20 12:15:11 +0200

    changing fruit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,6 +81,7 @@ puts ""
 
 # creating Products instances
 # VEGETABLES
+
 Product.create!(
     name: "Tomato",
     description: "Tomatoes are the Germans' favorite vegetable. From a botanical point of view, they belong to the berries. As a nightshade plant, they are a warmth-loving plant. Although tomatoes come in many shapes, the round ones are the most common in the world. Mainly from Spain, Italy, France, the Netherlands and Morocco. Fruity, piquant and spicy. Particularly suitable for salads.",
@@ -142,20 +143,66 @@ Product.create!(
     )
 
 # FRUITS
-5.times do
-    full_price = rand(1.0..4.5).round(2)
-    Product.create!(
-    name: Faker::Food.vegetables, 
-    description: Faker::Food.description, 
-    full_price: full_price,
+
+Product.create!(
+    name: "Mango",
+    description: "Mangoes grow on the tropical mango tree that is native to Central America, Africa and the Far East. The fruit peel can be green to red, is waxy and inedible. The color of the mango depends on the variety and has nothing to do with the degree of ripeness of the fruit. Growing area is Mainly Brazil, South Africa and Costa Rica. Sweet and sour with a peach aroma.",
+    full_price: rand(1.0..4.5).round(2),
     discounted_price: (full_price * 0.85).round(2),
-    best_before_date: Faker::Date.forward(days: 5),  
+    best_before_date: Faker::Date.forward(days: 5),
     category: "Fruits",
     amount: rand(1..10),
     unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
-    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd","Lidl" ].sample)
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
     )
- end
+
+Product.create!(
+    name: "Peaches",
+    description: "Peaches are very juicy and aromatic due to their nutritious and refreshing flesh. Mainly grown in Italy, Greece, Spain and France. Juicy and sweet, highly aromatic. White-fleshed varieties have the most intense taste. Mainly consumed raw, e.g. for fruit salads, or for compotes, cold bowls, punch bowls and jams.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Fruits",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
+
+Product.create!(
+    name: "Watermelon",
+    description: "The watermelon has a spherical or oval shape and can weigh up to 15 kg. Black seeds are embedded in the red pulp. Mainly grown in Spain. Juicy, refreshing, sweet, sometimes a bit bland.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Fruits",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
+
+Product.create!(
+    name: "Lemons",
+    description: "Lemon trees produce fruit all year round, so the lemons are available all year round. Mainly grown in Spain, Argentina, Italy and Turkey. Very sour. The pulp and juice are used, for example, for salad dressings, creams, baked goods and soft drinks. Halve the lemons and squeeze the two halves.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Fruits",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
+
+Product.create!(
+    name: "Pineapple",
+    description: "The pineapple fruit grows on a perennial plant and looks like a large pine cone. This is where the English name pineapple comes from. The fruit weighs 1-4 kg. Since pineapples only grow ideally under high temperatures, they are mainly grown in the tropics. Sweet with a fine acidity, juicy and aromatic.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Fruits",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
 
 puts "Finished creating #{Product.count} new Products"
 
