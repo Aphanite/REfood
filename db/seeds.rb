2ba508a7040495329f0e5cdb5b9f1bba3e7ef375
Zuzana Soltisova <zuzanasoltisova1@gmail.com>
2021-08-20 12:06:37 +0200

    changing veg seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -80,41 +80,68 @@ puts "Finished creating #{Supermarket.count} new Supermarkets"
 puts ""
 
 # creating Products instances
-# FRUITS
-5.times do
-    full_price = rand(1.0..4.5).round(2)
-    Product.create!(
-    name: Faker::Food.fruits, 
-    description: Faker::Food.description, 
-    full_price: full_price,
-    discounted_price: (full_price * 0.85).round(2),
-    best_before_date: Faker::Date.forward(days: 5),  
-    category: "Fruits",
-    amount: rand(1..10),
-    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
-    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
-    )
-end
-
-
+# VEGETABLES
 Product.create!(
     name: "Tomato",
-    description: "Country of origin and class may vary depending on the offer. Tomatoes are the Germans' favorite vegetable. From a botanical point of view, they belong to the berries. As a nightshade plant, they are a warmth-loving plant. Although tomatoes come in many shapes, the round tomato is the most common in the world. Mainly from Spain, Italy, France, the Netherlands and Morocco. Fruity, piquant and spicy. Particularly suitable for salads.",
+    description: "Tomatoes are the Germans' favorite vegetable. From a botanical point of view, they belong to the berries. As a nightshade plant, they are a warmth-loving plant. Although tomatoes come in many shapes, the round ones are the most common in the world. Mainly from Spain, Italy, France, the Netherlands and Morocco. Fruity, piquant and spicy. Particularly suitable for salads.",
     full_price: rand(1.0..4.5).round(2),
     discounted_price: (full_price * 0.85).round(2),
     best_before_date: Faker::Date.forward(days: 5),
-    category: "Fruits",
+    category: "Vegetables",
     amount: rand(1..10),
     unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
     supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
     )
 
+Product.create!(
+    name: "Cucumber",
+    description: "Cucumbers belong to the cucurbit family. Originally coming from the tropics, they have high temperature requirements and are almost exclusively planted in greenhouses. The fruits are about 30-40 cm long, slender and tapered at the ends. The skin is usually smooth and dark in color. Mainly grown in Greece, Spain, the Netherlands, Belgium and Germany.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Vegetables",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
 
+Product.create!(
+    name: "Broccoli",
+    description: "Broccoli is a vegetable related to cauliflower. Mainly grown in Germany, Italy, Spain, France, the Netherlands and England. More hearty than cauliflower, only a slight cabbage taste. Broccoli is prepared in a similar way to cauliflower.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Vegetables",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
 
+Product.create!(
+    name: "Carrots",
+    description: "Fresh carrots should be firm and crisp, with smooth and unblemished skin. Bright-orange colour indicates high carotene content; smaller types are the most tender. Carrots are used in salads and as relishes and are served as cooked vegetables and in stews and soups.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Vegetables",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
 
+Product.create!(
+    name: "Romaine Lettuce",
+    description: "Romaine lettuce has a tall, loose head. Its long, broad leaves are firm. The leaf margins are smooth, the central ribs are fleshy and pronounced. On the outside, the leaves are usually strong green and become lighter and yellowish towards the inside. Mainly grown in Germany, Italy, the Netherlands, France and Spain. Slightly tart, aromatic and hearty.",
+    full_price: rand(1.0..4.5).round(2),
+    discounted_price: (full_price * 0.85).round(2),
+    best_before_date: Faker::Date.forward(days: 5),
+    category: "Vegetables",
+    amount: rand(1..10),
+    unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
+    supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd", "Lidl" ].sample)
+    )
 
-
-# VEGETABLES
+# FRUITS
 5.times do
     full_price = rand(1.0..4.5).round(2)
     Product.create!(
@@ -123,7 +150,7 @@ Product.create!(
     full_price: full_price,
     discounted_price: (full_price * 0.85).round(2),
     best_before_date: Faker::Date.forward(days: 5),  
-    category: "Vegetables", 
+    category: "Fruits",
     amount: rand(1..10),
     unit: ["100 gr", "250 gr", "500 gr", "750 gr", "1000 gr"].sample,
     supermarket: Supermarket.find_by(name: ["Rewe", "Alnatura", "Aldi Süd","Lidl" ].sample)
