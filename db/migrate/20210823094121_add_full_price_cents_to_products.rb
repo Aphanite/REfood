7b8933013a792f5e2c4e7830ef6f3542ca0774ab
Zuzana Soltisova <zuzanasoltisova1@gmail.com>
2021-08-23 11:56:25 +0200

    conflicts

--- db/migrate/20210823094121_add_full_price_cents_to_products.rb
+++ db/migrate/20210823094121_add_full_price_cents_to_products.rb
@@ -1,4 +1,5 @@
 class AddFullPriceCentsToProducts < ActiveRecord::Migration[6.0]
-  def change
+    def change
+    add_monetize :products, :full_price_cents, currency: { present: false }
   end
 end
