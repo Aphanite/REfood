b39316bdfc0b5618f4f1b14513101e402ff222cf
Zuzana Soltisova <zuzanasoltisova1@gmail.com>
2021-08-28 12:35:41 +0200

    adding geocoding to model

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -3,5 +3,6 @@ class Supermarket < ApplicationRecord
   has_many :products
   has_many :supermarket_reviews
   has_many :ordered_items, through: :products
-
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
