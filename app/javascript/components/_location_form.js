28e873619affde682b8d67694c4fa81a80cad462
Zuzana Soltisova <zuzanasoltisova1@gmail.com>
2021-08-31 19:19:07 +0200

    editing javascript

--- app/javascript/components/_location_form.js
+++ app/javascript/components/_location_form.js
@@ -1,9 +1,10 @@
 const button = document.querySelector('.button-homepage')
 const locationForm = document.querySelector('.location-form')
-
-button.addEventListener('click', (event) => {
-locationForm.style.display = 'block';
+if (button) {
+  button.addEventListener('click', (event) => {
+    locationForm.style.display = 'block';
 })
+}
 
 
 
