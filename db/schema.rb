e12967204de61e632474930d2fffd01c91b12e90
Ena Rajovic <ena.rajovic@online.de>
2021-09-02 20:38:57 +0200

    adding total_price and discount to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_192141) do
+ActiveRecord::Schema.define(version: 2021_09_02_180132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,13 +26,12 @@ ActiveRecord::Schema.define(version: 2021_08_30_192141) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "status", default: 0
-    t.date "date"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "amount"
-    t.string "checkout_session_id"
     t.jsonb "pick_up_slots"
+    t.integer "total_price_cents"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
