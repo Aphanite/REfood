36d2af9e60afc32f09f99c261987d8af014dc9be
Ena Rajovic <ena.rajovic@online.de>
2021-09-11 13:05:15 +0200

    fix pwa

--- app/javascript/components/_location_form.js
+++ app/javascript/components/_location_form.js
@@ -3,6 +3,8 @@ const locationForm = document.querySelector('.location-form')
 if (button) {
   button.addEventListener('click', (event) => {
     locationForm.style.display = 'block';
+    const homeScreen = document.getElementById("home-screen");
+    homeScreen.hidden = true;
 })
 }
 
